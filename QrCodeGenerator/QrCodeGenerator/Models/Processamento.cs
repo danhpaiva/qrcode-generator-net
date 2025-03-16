c3160e5650280b5cc2dfc8e7fd856b2c0b6047a7
Paiva <danpaiva@live.com>
2025-03-16 11:42:27 -0300

    refact: refactoring code

---
+++ QrCodeGenerator/QrCodeGenerator/Models/Processamento.cs
@@ -0,0 +1,41 @@
+﻿using QRCoder;
+
+namespace QrCodeGenerator.Models;
+
+public class Processamento
+{
+    public static bool ValidarUrl(string url)
+    {
+        if (string.IsNullOrEmpty(url))
+        {
+            Console.WriteLine("A url está inválida...");
+            return false;
+        }
+        return true;
+    }
+
+    public static void GerarImagem(string url)
+    {
+        try
+        {
+            QRCodeGenerator qRCodeGenerator = new();
+            QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+
+            using (PngByteQRCode qrCode = new PngByteQRCode(qRCodeData))
+            {
+                byte[] qrCodeImage = qrCode.GetGraphic(40);
+
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                string filePath = Path.Combine(desktopPath, "qrCode.png");
+
+                File.WriteAllBytes(filePath, qrCodeImage);
+
+                Console.WriteLine($"QrCode gerado com sucesso no diretorio: " + filePath);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Aconteceu um erro ao gerar um QrCode: " + e.Message);
+        }
+    }
+}
--- QrCodeGenerator/QrCodeGenerator/Program.cs
+++ QrCodeGenerator/QrCodeGenerator/Program.cs
@@ -1,32 +1,9 @@
-﻿using QRCoder;
+﻿using QrCodeGenerator.Models;
 
 Console.WriteLine("Digite a URL para gerar o QrCode: ");
 string url = Console.ReadLine();
 
-if (string.IsNullOrEmpty(url))
-{
-    Console.WriteLine("A url está inválida...");
-    return;
-}
+var resultadoProcessamento = Processamento.ValidarUrl(url);
 
-try
-{
-    QRCodeGenerator qRCodeGenerator = new();
-    QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
-
-    using (PngByteQRCode qrCode = new PngByteQRCode(qRCodeData))
-    {
-        byte[] qrCodeImage = qrCode.GetGraphic(40);
-
-        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-        string filePath = Path.Combine(desktopPath, "qrCode.png");
-
-        File.WriteAllBytes(filePath, qrCodeImage);
-
-        Console.WriteLine($"QrCode gerado com sucesso no diretorio: " + filePath);
-    }
-}
-catch (Exception e)
-{
-    Console.WriteLine("Aconteceu um erro ao gerar um QrCode: " + e.Message);
-}
+if (resultadoProcessamento.Equals(true))
+    Processamento.GerarImagem(url);
